6c8484a33c1cfc52a017dd6cc536ff6048333b01
Nicolly Reis <nicollyro2004@gmail.com>
2023-09-06 22:00:32 -0300

    Estiliza a tela do app, e cria helpers para obter informações

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { getOpenWeaherLink } from './helper';
+import axios from 'axios';
+import { LinearGradient } from 'expo-linear-gradient';
+import Icon  from 'react-native-vector-icons/Entypo';
 
 export default function App() {
+  const [city,setCity] = useState("");
+  const[weather,setWeather] = useState({});
+
+  const getCityWeather = async () => {
+    const {data: {main,weather,wind:{speed},
+  },
+ } = await axios.get(getOpenWeaherLink(city));
+
+ setWeather({
+  wind_speed: speed,
+  weather_type: weather[0].main,
+  temp: main.temp,
+  humidity: main.humidity,
+  feels_like: main.feels_like,
+});
+
+};
+
+  useEffect(() => {
+    if(city === "") return;
+    getCityWeather();
+  },[city]);
+
   return (
+    <LinearGradient colors={["#22b3fc", "#bfbfbf"]}
+    style={{
+      flex: 1,
+    }}
+    >
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <View style={styles.seachForm}>
+          <TextInput style={styles.seachInput}/>
+          <Pressable style={styles.seachButton}>
+            <Icon name="magnifying-glass" size={25}/>
+          </Pressable>
+        </View>
+      </View>
     </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  seachInput: {
+    width: "84%",
+    backgroundColor: "#eee",
+    padding: 4,
+    borderRadius:7,
+  },
+  seachButton:{
+    width: "100%",
+    alignItems:"center",
+    flex: 1,
+    padding:10,
+  },
+  seachForm: {
+    flexDirection:"row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 4,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: "row"
+  },
+  card:{
+    padding:10,
+    flex: 1,
+    flexDirection: "column",
+    gap: 5,
+    backgroundColor: "#f9f9fb",
+    borderRadius:7,
+    marginHorizontal: 10,
+  },
+  cardButton:{
+    padding:5,
   },
 });
---
+++ helper.ts
@@ -0,0 +1,20 @@
+const API_KEY = '69b5a50afb7b18d2fd5d1f7d3fd45a5a'
+
+
+export const getOpenWeaherLink = (cidade: string) => {
+ return `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${API_KEY}&lang=pt_br`;
+}
+
+
+export const weatherClassification= (weather: "Clear"| "Clouds"| "Mist"| "Rain"|"Snow") => {
+
+ const imagens = {
+    Clear: require('./assets/clear.png'),
+    Clouds: require('./assets/cloud.png'),
+    Mist: require('./assets/mist.png'),
+    Rain: require('./assets/rain.png'),
+    Snow: require('./assets/snow.png'),
+};
+
+return imagens[weather] ? imagens[weather] : imagens['Clouds']
+}
